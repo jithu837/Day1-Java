e180aaad6d337b7254f978e6509cebd5506db116
jithu837 <jithendrasolanki56@gmail.com>
2024-12-02 15:19:22 +0530

    Add files via upload

---
+++ printingnextchar.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class printingnextchar
+{
+    public static void main(String args[])
+    {
+        //conversion of ascii value to char 
+        int a=65;
+        char b='A';
+        b+=1;
+        char v=(char)b;
+        System.out.println(v);
+    }
+}
