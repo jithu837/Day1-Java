891d2a8d84706c87682cf6c02d0987188d1dfadf
jithu837 <jithendrasolanki56@gmail.com>
2024-12-02 14:37:33 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class datatypes
+{
+    public static void main(String args[])
+    {
+        int a=12;
+        char b='A';
+        float c=3.07f;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
