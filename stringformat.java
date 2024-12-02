412aa0c67d8f1675d38747424f5818ea3ec24e3f
jithu837 <jithendrasolanki56@gmail.com>
2024-12-02 16:23:24 +0530

    Add files via upload

---
+++ stringformat.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class stringformat
+{
+    public static void main(String args[])
+    {
+        //Accepting a String
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        int c=sc.nextInt();
+        System.out.println("The name of the person is "+a+" and age is "+c);
+    }
+}
