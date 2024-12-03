62124ddac60e58b65c3d5421706187557ccfb951
jithu837 <jithendrasolanki56@gmail.com>
2024-12-03 10:41:04 +0530

    Add files via upload

---
+++ AcceptingString.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class AcceptingString
+{
+    public static void main(String args[])
+    {
+        //Accepting a String
+        Scanner sc = new Scanner (System.in);
+        String a=sc.nextLine();
+        String b=sc.nextLine();
+        String c=sc.nextLine();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+
+    }
+}
